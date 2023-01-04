b4a582fdfa71397ae5679c2b6918ad3a2f4aa889
marcsedo01@gmail.com <marcsedo01@gmail.com>
2023-01-04 12:34:54 +0000

    Set up dim_customers

--- models/dimensions/dim_customers.sql
+++ models/dimensions/dim_customers.sql
@@ -1,8 +1,9 @@
+
 select 
 
-    customer_name,
-    max(customer_rating_food)  as "Max customer rating food",
-    max(customer_rating_delivery) as "Max customer rating delivery"
+customer_name,
+max(customer_rating_food)  as "Max customer rating food",
+max(customer_rating_delivery) as "Max customer rating delivery"
 
 from {{ref('stg_orders')}}
 
